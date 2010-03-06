e253dc46288ea698e0019e171b9ab5f203ff6dcc
Will Hui <wth4@cornell.edu>
2010-03-06 15:13:17 -0500

    User-wide gitignore configuration.

--- restore-dotfiles.sh
+++ restore-dotfiles.sh
@@ -7,6 +7,7 @@ ln -snf $CWD/vimrc ~/.vimrc
 ln -snf $CWD/emacs ~/.emacs
 ln -snf $CWD/emacs.d ~/.emacs.d
 ln -snf $CWD/gitconfig ~/.gitconfig
+ln -snf $CWD/gitignore ~/.gitignore
 ln -snf $CWD/zshrc ~/.zshrc
 ln -snf $CWD/bashrc ~/.bashrc
 ln -snf $CWD/inputrc ~/.inputrc
